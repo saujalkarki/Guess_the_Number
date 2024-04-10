16635b1a4ed785df159e403cb2bb6229f01ba6f0
Saujal Karki <saujalkarki04@gmail.com>
2024-04-10 21:02:05 +0545

    Guess The Number Logic Done

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 // Generating a random Number
 const randomNumber = Math.floor(Math.random() * 20) + 1;
 console.log(randomNumber);
-console.log(typeof randomNumber);
 
 let score = 20;
 
 let btnSubmit = document.querySelector(".Submit");
-
 let message = document.querySelector(".message");
 let score1 = document.querySelector(".Score");
 
 btnSubmit.addEventListener("click", function () {
+  let inpFld = document.querySelector("#guessedNumber");
   let guessedNumber = parseInt(document.querySelector("#guessedNumber").value);
 
   if (randomNumber === guessedNumber) {
-    console.log("Congratulations! You guessed the correct Number");
+    message.textContent =
+      "Congratulations! You have guessed the correct Number";
+    document.querySelector("body").style.backgroundColor = "cyan";
   } else {
     if (randomNumber < guessedNumber) {
-      console.log(guessedNumber);
-      console.log("You guessed it high");
+      message.textContent = "You guessed it high";
     } else {
-      console.log("You guessed it low");
+      message.textContent = "You guessed it low";
     }
+    score--;
+    if (score === 0) {
+      inpFld.disabled = true;
+      message.textContent = `You lose the game. The correct random Number is ${randomNumber}`;
+    }
+    score1.textContent = score;
   }
 });
